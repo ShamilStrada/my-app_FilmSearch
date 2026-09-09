9c39f075206d1237197a5e955577842bac0b695d
ShamilStrada <5000mmr02@mail.ru>
2026-09-09 16:15:04 +0500

    Поигрались с CSSбокса для найстройки других экранов

--- src/25Task/App.tsx
+++ src/25Task/App.tsx
@@ -1,94 +1,99 @@
-import FormControl from "@mui/material/FormControl";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { useState } from "react";
-import { MyAutocomplete } from "./MyAutocomplete";
-import { MySlider } from "./SliderMy";
-import { MyFilter } from "./Filter";
-import { MyCard } from "./Card";
-import IconButton from "@mui/material/IconButton";
-import ClearIcon from "@mui/icons-material/Clear";
-import { MyPagination } from "./MyPagination";
-import { InputFilm } from "./InputFilm";
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import { MyAutocomplete } from './MyAutocomplete';
+import { MySlider } from './SliderMy';
+import { MyFilter } from './Filter';
+import { MyCard } from './Card';
+import IconButton from '@mui/material/IconButton';
+import ClearIcon from '@mui/icons-material/Clear';
+import { MyPagination } from './MyPagination';
+import { InputFilm } from './InputFilm';
 
 export function App() {
-  const [page, setPage] = useState<number>(1);///страница 
-  const [stateFilter, usestateFilter] = useState<boolean>(false);///обновление фильтров 
-  const [results, setResults] = useState<[]>([]);///массив с данными 
-  const [value, setValue] = useState<number[]>([1950, 2026]);///слайдер
-  const [filterAutocomplete, setFilterAutocomplete] = useState<[]>([])//фильтры жанров
-  const [dataSearchFilm, setDataSearchFilm] = useState<[]>([])//поиск по названию
-  const funSearchFilm=(list:[])=>{
+  const [page, setPage] = useState<number>(1); ///страница
+  const [stateFilter, usestateFilter] = useState<boolean>(false); ///обновление фильтров
+  const [results, setResults] = useState<[]>([]); ///массив с данными
+  const [value, setValue] = useState<number[]>([1950, 2026]); ///слайдер
+  const [filterAutocomplete, setFilterAutocomplete] = useState<[]>([]); //фильтры жанров
+  const [dataSearchFilm, setDataSearchFilm] = useState<[]>([]); //поиск по названию
+  const funSearchFilm = (list: []) => {
     setDataSearchFilm(list);
-  }
-  const funChangeApp=(list:[])=>{
+  };
+  const funChangeApp = (list: []) => {
     setFilterAutocomplete(list);
-  }
-  function handleChange(event:any, value:number[]) {///функция на изменение времени слайдера 
+  };
+  function handleChange(event: any, value: number[]) {
+    ///функция на изменение времени слайдера
     setValue(value);
   }
-  function ChangeResults(list: []) {///функция изменения результатов запроса  
+  function ChangeResults(list: []) {
+    ///функция изменения результатов запроса
     setResults(list);
   }
-  function handleChangePage(event:any, value:number) {///функц. изменения номера страницы
+  function handleChangePage(event: any, value: number) {
+    ///функц. изменения номера страницы
     setPage(value);
   }
   if (stateFilter) {
     return (
       <>
         <FormControl fullWidth sx={{ m: 1 }} />
-         <Box
+        <Box
           sx={{
-            width: 420,
-            ml: 1,
-            border: "3px dashed",
-            position: "absolute", //позиция
+            width: '25%',
+            height: '100vh',
+            m: 1,
+            border: '2px dashed',
+            position: 'absolute', //позиция
             top: 0, //перенесли бокс в правый верхний угол
             left: 0,
-            display: "block", //отображение
+            display: 'flex', //отображение
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
           }}
         >
-          <Box sx={{ mb: 1, display: "flex", gap: 2 }}>
-            <Typography sx={{ fontSize: 25, fontStyle: "italic" }}>
-              Фильтры
-            </Typography>
+          <Box sx={{ mb: 1, display: 'flex', gap: 2 }}>
+            <Typography sx={{ fontSize: 25, fontStyle: 'italic' }}>Фильтры</Typography>
             <IconButton onClick={() => usestateFilter(!stateFilter)}>
               <ClearIcon></ClearIcon>
             </IconButton>
           </Box>
-          <Box sx={{ mb: 1, display: "block", gap: 2 }}>
+          <Box sx={{ mb: 1, display: 'block', gap: 2 }}>
             <InputFilm page={page} funChangeFilm={funSearchFilm}></InputFilm>
           </Box>
-          <MyFilter
-            state={results}
-            handleState={ChangeResults}
-            page={page}
-          ></MyFilter>
+          <MyFilter state={results} handleState={ChangeResults} page={page}></MyFilter>
           <Typography sx={{ mb: 5 }}>Год релиза:</Typography>
           <MySlider value={value} handleChange={handleChange}></MySlider>
           <FormControl fullWidth sx={{ m: 1 }}></FormControl>
-          <MyAutocomplete state={filterAutocomplete} funChange={funChangeApp}></MyAutocomplete>
-          <MyPagination
-            page={page}
-            handleChangePage={handleChangePage}
-          ></MyPagination>
+          <MyAutocomplete
+            state={filterAutocomplete}
+            funChange={funChangeApp}
+          ></MyAutocomplete>
+          <MyPagination page={page} handleChangePage={handleChangePage}></MyPagination>
         </Box>
         <Box
           sx={{
-            border: "3px dashed",
-            width: 1200,
-            height: 800,
-            position: "fixed", //позиция
+            border: '3px dashed',
+            width: '70%',
+            height: '100%',
+            position: 'fixed', //позиция
             top: 0, //перенесли бокс в правый верхний угол
             right: 0,
             m: 1, //отступы
-            display: "flex", //отображение
-            flexWrap: "wrap", //перенос строки
-            overflow: "auto",
+            display: 'flex', //отображение
+            flexWrap: 'wrap', //перенос строки
+            overflow: 'auto',
           }}
         >
           kkkk
-          <MyCard searchResults={dataSearchFilm}  listResults={results} value={value} filterAutocompleteCard={filterAutocomplete}></MyCard>
+          <MyCard
+            searchResults={dataSearchFilm}
+            listResults={results}
+            value={value}
+            filterAutocompleteCard={filterAutocomplete}
+          ></MyCard>
         </Box>
       </>
     );
@@ -102,52 +107,50 @@ export function App() {
           sx={{
             width: 420,
             ml: 1,
-            border: "3px dashed",
-            position: "absolute", //позиция
+            border: '3px dashed',
+            position: 'absolute', //позиция
             top: 0, //перенесли бокс в правый верхний угол
             left: 0,
-            display: "block", //отображение
+            display: 'block', //отображение
           }}
         >
-          <Box sx={{ mb: 1, display: "flex", gap: 2 }}>
-            <Typography sx={{ fontSize: 25, fontStyle: "italic" }}>
-              Фильтры
-            </Typography>
+          <Box sx={{ mb: 1, display: 'flex', gap: 2 }}>
+            <Typography sx={{ fontSize: 25, fontStyle: 'italic' }}>Фильтры</Typography>
             <IconButton onClick={() => usestateFilter(!stateFilter)}>
               <ClearIcon></ClearIcon>
             </IconButton>
-            
           </Box>
           <InputFilm page={page} funChangeFilm={funSearchFilm}></InputFilm>
-          <MyFilter
-            state={results}
-            handleState={ChangeResults}
-            page={page}
-          ></MyFilter>
+          <MyFilter state={results} handleState={ChangeResults} page={page}></MyFilter>
           <Typography sx={{ mb: 5 }}>Год релиза:</Typography>
           <MySlider value={value} handleChange={handleChange}></MySlider>
           <FormControl fullWidth sx={{ m: 1 }}></FormControl>
-          <MyAutocomplete state={filterAutocomplete} funChange={funChangeApp}></MyAutocomplete>
-          <MyPagination
-            page={page}
-            handleChangePage={handleChangePage}
-          ></MyPagination>
+          <MyAutocomplete
+            state={filterAutocomplete}
+            funChange={funChangeApp}
+          ></MyAutocomplete>
+          <MyPagination page={page} handleChangePage={handleChangePage}></MyPagination>
         </Box>
         <Box
           sx={{
-            border: "3px dashed",
+            border: '3px dashed',
             width: 1200,
             height: 850,
-            position: "absolute", //позиция
+            position: 'absolute', //позиция
             top: 0, //перенесли бокс в правый верхний угол
             right: 0,
             m: 1, //отступы
-            display: "flex", //отображение
-            flexWrap: "wrap", //перенос строки
-            overflow: "auto",
+            display: 'flex', //отображение
+            flexWrap: 'wrap', //перенос строки
+            overflow: 'auto',
           }}
         >
-          <MyCard searchResults={dataSearchFilm}  listResults={results} value={value} filterAutocompleteCard={filterAutocomplete}></MyCard>
+          <MyCard
+            searchResults={dataSearchFilm}
+            listResults={results}
+            value={value}
+            filterAutocompleteCard={filterAutocomplete}
+          ></MyCard>
         </Box>
       </>
     );
--- src/25Task/MyPagination.tsx
+++ src/25Task/MyPagination.tsx
@@ -11,7 +11,7 @@ interface MyPaginationProps{
 export function MyPagination({page,handleChangePage}:MyPaginationProps) {  
   return (
     <>
-      <Box sx={{ pt: 50 }}>
+      <Box sx={{ display:"flex", pt: 50, position:'' }}>
         <Stack spacing={1}>
           <Typography>Страница:{page}</Typography>
           <Pagination
